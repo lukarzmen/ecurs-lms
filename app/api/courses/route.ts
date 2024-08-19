0623f1d86f21995873e82efb1012df6625ad2bc8
lmedyk <Lukasz.Medyk@bffgroup.com>
2024-08-19 14:21:34 +0200

    feat: db init

---
+++ app/api/courses/route.ts
@@ -0,0 +1,35 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+    request: Request;
+    {
+        try {
+            const {userId} = auth() ?? "";
+            const {title} = await req.json();
+
+    
+            if(!userId){
+                return new NextResponse("Unauthorized", {
+                    status: 401
+                });
+            }
+            const course = await db.course.create({
+                data: {
+                    userId,
+                    title
+                }
+            });
+            const response = new NextResponse(JSON.stringify(course), {
+                status: 201
+            });
+            return response;
+        } catch (error) {
+            return new NextResponse("Internal error" , {
+                status: 500
+            });
+        }
+    }
+    
+}
--- readme.md
+++ readme.md
@@ -34,5 +34,6 @@ The easiest way to deploy your Next.js app is to use the [Vercel Platform](https
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
 
 
-## Utilities
+## Tools used
 https://ui.shadcn.com/
+https://clerk.com/
