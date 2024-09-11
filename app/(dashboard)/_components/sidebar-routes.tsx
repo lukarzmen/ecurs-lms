b33b0df75008f96688a5dc35e9c80755828261e6
lmedyk <Lukasz.Medyk@bffgroup.com>
2024-09-11 11:03:12 +0200

    test: format. testing new github accout priviledges

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -31,16 +31,16 @@ const teacherRoutes = [
 ]
 export const SidebarRoutes = () => {
     const pathName = usePathname();
-    
+
     const isTeacherPage = pathName?.startsWith("/teacher");
 
     const routes = isTeacherPage ? teacherRoutes : guestRoutes;
-    return ( <div className="flex flex-col w-full">
+    return (<div className="flex flex-col w-full">
         {
             routes.map((route) => {
                 return (
                     <SidebarItem key={route.href} icon={route.icon} label={route.label} href={route.href}>
-            
+
                     </SidebarItem>
                 );
             })
--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -4,10 +4,10 @@ import { SidebarRoutes } from "./sidebar-routes"
 export const Sidebar = () => {
     return (<div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
         <div className="p-6">
-            <Logo/>
+            <Logo />
         </div>
         <div className="flex flex-col w-full">
-            <SidebarRoutes/>
+            <SidebarRoutes />
         </div>
     </div>)
 }
