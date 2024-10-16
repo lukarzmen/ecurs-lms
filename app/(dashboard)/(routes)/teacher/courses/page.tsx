a801cbe205bea23fd2c7e0cc917bc3343838beff
lukarzmen <lukarzmen@gmail.com>
2024-10-16 14:00:22 +0200

    feat: courses list table

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,13 +1,31 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
-const CoursesPage = () => {
+const CoursesPage = async () => {
+
+  const {userId} = auth();
+  if(!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId: userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
-    <div className="p-6">
-      <Link href="/teacher/courses/create">
-        <Button>New course</Button>
-      </Link>
-    </div>
+      <div className="p-6">
+        <DataTable columns={columns} data={courses} />
+      </div>
   );
 };
 
