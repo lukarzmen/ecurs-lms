8af46147f584bc630b86996e583985695cd817b0
lukarzmen <lukarzmen@gmail.com>
2024-10-23 23:10:17 +0200

    feat: poc lexical editor extensions

--- app/(dashboard)/(routes)/editor/_nodes/TextAreaNode.tsx
+++ app/(dashboard)/(routes)/editor/_nodes/TextAreaNode.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 // Define a React component that will represent the textarea in the editor
 function TextAreaComponent() {
-  return <textarea className="textarea-node" placeholder="Enter text here" />;
+  return <textarea className="textarea-node flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" placeholder="Enter text here" />;
 }
 
 export class TextAreaNode extends DecoratorNode<{}> {
--- app/(dashboard)/(routes)/editor/page.tsx
+++ app/(dashboard)/(routes)/editor/page.tsx
@@ -26,6 +26,13 @@ const editorConfig = {
 };
 
 export default function EditorPage() {
+  //youtube & image plugin
+  //https://codesandbox.io/p/sandbox/lexical-youtube-plugin-example-5unxt3?file=%2Fsrc%2Fplugins%2FYouTubePlugin.ts
+  //https://codesandbox.io/p/sandbox/lexical-image-plugin-example-iy2bc5?file=%2Fsrc%2FApp.js
+
+  //other plugins https://codesandbox.io/examples/package/lexical
+  //this is interesting https://playground.lexical.dev/
+
   return (
     <div className="p-6">
       <LexicalComposer initialConfig={editorConfig}>
