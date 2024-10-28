5aea8c0107ae17054a8ffae70e3f5fb8436969dc
lukarzmen <lukarzmen@gmail.com>
2024-10-28 23:23:31 +0100

    feat: lexical editor added

--- app/(dashboard)/(routes)/editor/page.tsx
+++ app/(dashboard)/(routes)/editor/page.tsx
@@ -1,29 +1,7 @@
 "use client";
 
-import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
-import { LexicalComposer } from "@lexical/react/LexicalComposer";
-import { ContentEditable } from "@lexical/react/LexicalContentEditable";
-import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
-import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
-import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import PlaygroundApp from "@/components/editor/App";
 
-import ExampleTheme from "./ExampleTheme";
-import ToolbarPlugin from "./_plugins/ToolbarPlugin";
-import TreeViewPlugin from "./_plugins/TreeViewPlugin";
-import "./styles.css";
-import { TextAreaNode } from "./_nodes/TextAreaNode";
-const placeholder = "Enter some rich text...";
-
-const editorConfig = {
-  namespace: "React.js Demo",
-  nodes: [TextAreaNode],
-  // Handling of errors during update
-  onError(error: Error) {
-    throw error;
-  },
-  // The editor theme
-  theme: ExampleTheme,
-};
 
 export default function EditorPage() {
   //youtube & image plugin
@@ -35,28 +13,7 @@ export default function EditorPage() {
 
   return (
     <div className="p-6">
-      <LexicalComposer initialConfig={editorConfig}>
-        <div className="editor-container">
-          <ToolbarPlugin />
-          <div className="editor-inner">
-            <RichTextPlugin
-              contentEditable={
-                <ContentEditable
-                  className="editor-input"
-                  aria-placeholder={placeholder}
-                  placeholder={
-                    <div className="editor-placeholder">{placeholder}</div>
-                  }
-                />
-              }
-              ErrorBoundary={LexicalErrorBoundary}
-            />
-            <HistoryPlugin />
-            <AutoFocusPlugin />
-            <TreeViewPlugin />
-          </div>
-        </div>
-      </LexicalComposer>
+      <PlaygroundApp></PlaygroundApp>
     </div>
   );
 }
