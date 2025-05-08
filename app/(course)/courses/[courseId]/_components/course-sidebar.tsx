5332be67f6e94c99dc5ced5ee3ce52af8375beb2
lukarzmen <lukarzmen@gmail.com>
2025-05-08 23:29:10 +0200

    fix: async not only available in client side component

--- app/(course)/courses/[courseId]/_components/course-sidebar.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar.tsx
@@ -1,22 +1,29 @@
+"use client"; // Mark as a Client Component
+
 import CourseSidebarItem from "./course-sidebar-item";
 import { CourseSidebarProps } from "./course-mobile-sidebar";
 
-
-export const CourseSidebar = async ({ course }: CourseSidebarProps) => {
+export const CourseSidebar = ({ course }: CourseSidebarProps) => {
     return (
         <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm rounded-md">
             <div className="p-8 flex flex-col border-b">
                 <h1 className="font-semibold">
                     {course.title}
                 </h1>
-                </div>
-                <div className="flex flex-col w-full">
-                    {course.modules && course.modules.map(
-                        (chapter) => (
-                            <CourseSidebarItem key={chapter.id} id={chapter.id} label={chapter.title} courseId={course.id} progressState={chapter.progressState}/>
-                        ))}
-                </div>
-          
+            </div>
+            <div className="flex flex-col w-full">
+                {course.modules && course.modules.map(
+                    (chapter) => (
+                        <CourseSidebarItem
+                            key={chapter.id}
+                            id={chapter.id}
+                            label={chapter.title}
+                            courseId={course.id}
+                            progressState={chapter.progressState}
+                        />
+                    )
+                )}
+            </div>
         </div>
-    )
+    );
 };
