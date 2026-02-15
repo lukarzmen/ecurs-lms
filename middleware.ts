a51abb7f6ad8872a42f4b5aafcecd10e28059226
lukarzmen <lukarzmen@gmail.com>
2026-02-15 22:56:27 +0100

    fix: cors

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,13 @@
 
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
-
-import { NextResponse } from 'next/server';
-
-export default clerkMiddleware(async (auth, request) => {
-
-  await auth();
-  request.headers.set('Access-Control-Allow-Origin', 'http://localhost:3000');
-  request.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  request.headers.set('Access-Control-Allow-Headers', '*');
-  return NextResponse.next();
-  
-})
-
-
+export default clerkMiddleware()
 
 export const config = {
-  matcher: ['/((?!_next).*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
--- next.config.js
+++ next.config.js
@@ -12,4 +12,31 @@ module.exports = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: process.env.NODE_ENV === 'production' 
+              ? 'https://ecurs.pl,https://clerk.ecurs.pl,https://accounts.ecurs.pl' 
+              : 'http://localhost:3000',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Content-Type, Authorization, X-Clerk-Auth-Token',
+          },
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true',
+          },
+        ],
+      },
+    ];
+  },
 };
